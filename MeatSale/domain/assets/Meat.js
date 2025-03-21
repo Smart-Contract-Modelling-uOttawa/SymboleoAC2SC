1ae565b8a5f884c1ffefd94036b220d90dee200b
Sofana Alfuhaid <45871455+SofanaAlfuhaid@users.noreply.github.com>
2025-03-20 22:07:59 -0400

    Add files via upload

--- MeatSale/domain/assets/Meat.js
+++ MeatSale/domain/assets/Meat.js
@@ -1,1 +1,11 @@
+const { PerishableGood } = require("./PerishableGood.js");
+//Sofana-AC add _owner for controller
+class Meat extends PerishableGood {
+  constructor(_name,owner,quantity, quality) {
+    super(_name,owner,quantity, quality)
+    this._type = "Meat"
 
+  }
+}
+
+module.exports.Meat = Meat
