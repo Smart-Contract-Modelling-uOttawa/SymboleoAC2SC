9a2c1ba87d2322ec568f8e860461d477ce5d4f8b
Sofana Alfuhaid <45871455+SofanaAlfuhaid@users.noreply.github.com>
2025-08-21 17:58:05 -0400

    Add files via upload

--- MeatSale/domain/assets/Meat.js
+++ MeatSale/domain/assets/Meat.js
@@ -1,10 +1,11 @@
 const { PerishableGood } = require("./PerishableGood.js");
-//Sofana-AC add _owner for controller
-class Meat extends PerishableGood {
-  constructor(_name,owner,quantity, quality) {
-    super(_name,owner,quantity, quality)
-    this._type = "Meat"
+const { Attribute } = require("symboleoac-js-core");
 
+class Meat extends PerishableGood {
+  constructor(_name,quantity, quality, barcode, owner) {
+    super(_name,quantity, quality, barcode, owner)
+    this._type = "Meat"
+    
   }
 }
 
--- MeatSale/domain/assets/PerishableGood.js
+++ MeatSale/domain/assets/PerishableGood.js
@@ -1,18 +1,14 @@
-const { Asset } = require("symboleo-js-core");
-const { Attribute } = require("/Users/sfuhaid/Symboleo2SC-demo-mainFunctionACcore/node_modules/symboleo-js-core/core/Attribute.js");
-//Sofana-AC add _owner for controller
-/**code genration add  */
+const { Asset } = require("symboleoac-js-core");
+const { Attribute } = require("symboleoac-js-core");
+
 class PerishableGood extends Asset {
-  constructor(_name,owner,quantity, quality) {
-    super(_name,owner)
+  constructor(_name,quantity, quality, barcode, owner) {
+    super(owner)
     this._name = _name
-    //this._owners = owner
-    //this.quantity = quantity
-    //this.quality = quality
-    //new enhancment for AC when we add attribute as a class extendes from Resource
-    this._owners = new Attribute("owner",owner,_name);  
-    this.quantity = new Attribute("quantity",quantity,_name); 
-    this.quality = new Attribute("quality",quality,_name); 
+    this._type = "PerishableGood"
+    this.quantity = new Attribute("quantity",quantity)
+    this.quality = new Attribute("quality",quality)
+    this.barcode = new Attribute("barcode",barcode)
   }
 }
 
